d1d5e110342a7524ebb207cc36ea90ce42cb755c
AlejoSuarezRuiz <55962709+AlejoSuarezRuiz@users.noreply.github.com>
2019-10-02 11:37:10 -0500

    Add files via upload

--- ws_server.py
+++ ws_server.py
@@ -1,6 +1,7 @@
 import os
 import time
 import socket
+import copy
 from flask import Flask, render_template
 from flask_sockets import Sockets
 
@@ -8,6 +9,7 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_address = ('0.0.0.0', 8500)
 sock.bind(server_address)
 sock.listen(1)
+Daq_data = 0
 
 app = Flask(__name__)
 sockets = Sockets(app)
@@ -16,19 +18,21 @@ sockets = Sockets(app)
 def dataChanel(ws):
     """Send data."""
     while not ws.closed:
-        connection, client_address = sock.accept()
-        try:
-            msj = ""
-            while True:
-                data = connection.recv(1).decode("utf-8")
-                if data and data != "\n":
-                    msj += data
-                elif data == "\n":
-                    print(msj)
-                    ws.send(msj)
-                    msj = ""     
-        finally:
-            connection.close()
+        ws.send(Daq_data)
+        time.sleep(0.05)
+        # connection, client_address = sock.accept()
+        # try:
+        #     msj = ""
+        #     while True:
+        #         data = connection.recv(1).decode("utf-8")
+        #         if data and data != "\n":
+        #             msj += data
+        #         elif data == "\n":
+        #             print(msj)
+        #             ws.send(msj)
+        #             msj = ""     
+        # finally:
+        #     connection.close()
 
 @sockets.route('/submit')
 def dataChanel(ws):
@@ -41,6 +45,24 @@ def dataChanel(ws):
 @app.route('/')
 def hello():
     return 'Hello World!'
+
+@app.route('/create_connection')
+def hello():
+    connection, client_address = sock.accept()
+    try:
+        msj = ""
+        while True:
+            data = connection.recv(1).decode("utf-8")
+            if data and data != "\n":
+                msj += data
+            elif data == "\n":
+                Daq_data = copy.copy(msj)
+                print(Daq_data)
+                msj = ""     
+    finally:
+        connection.close()
+        return 'Closed'
+    
             
 if __name__ == "__main__":
     from gevent import pywsgi
